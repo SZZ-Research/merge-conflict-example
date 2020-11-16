4aeaa9d43c8bc5c42d53869d493cccdd845fa2b2
luisamaralh <luis.amaralh@gmail.com>
2020-11-16 12:16:41 -0300

    Added product expiration date

--- src/com/unb/models/Product.java
+++ src/com/unb/models/Product.java
@@ -3,6 +3,7 @@ package com.unb.models;
 public class Product {
     private float price;
     private String name;
+    private String expirationDate;
 
     public void setPrice(float price){
         this.price = price;
@@ -12,11 +13,19 @@ public class Product {
         this.name = name;
     }
 
+    public void setExpirationDate(String name){
+        this.name = name;
+    }
+
     public float getPrice(){
         return this.price;
     }
 
     public String getName(){
         return this.name;
+    }
+    
+    public float getExpirationDate(){
+        return this.price;
     }
 }
