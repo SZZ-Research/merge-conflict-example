c8e25a59cb111d3da8ea979f8310e433c7596537
zfortes <jose.fortes.neto@gmail.com>
2020-11-16 11:38:35 -0300

    bugfix

--- src/com/unb/services/Register.java
+++ src/com/unb/services/Register.java
@@ -1,4 +1,6 @@
 package com.unb.services;
+import java.util.LinkedList;
+import java.util.Locale;
 import java.util.Scanner;
 
 import com.unb.models.Buyer;
@@ -9,9 +11,12 @@ import com.unb.models.Saller;
 public class Register {
     public Goods saller(){
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
         System.out.println("Enter number of products");
         int nProducts = scanner.nextInt();
         Goods goods = new Goods();
+        goods.setProducts(new LinkedList<>());
+        scanner.nextLine();
         for (int i = 0; i < nProducts; i++){
             Product product = new Product();
             System.out.println("Insert product name");
@@ -21,6 +26,7 @@ public class Register {
             float price = scanner.nextFloat();
             product.setPrice(price);
             goods.addProduct(product);
+            scanner.nextLine();
         }
         System.out.println("Enter a buyer name");
         Buyer buyer = new Buyer();
